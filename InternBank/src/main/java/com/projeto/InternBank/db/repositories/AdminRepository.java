8a25d1569869ac77b6b186529a62be565e7cfd72
lorena <dlm.lorena@gmail.com>
2025-02-04 14:29:08 -0300

    Update AdminRepository.java

--- InternBank/src/main/java/com/projeto/InternBank/db/repositories/AdminRepository.java
+++ InternBank/src/main/java/com/projeto/InternBank/db/repositories/AdminRepository.java
@@ -3,8 +3,11 @@ package com.projeto.InternBank.db.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.projeto.InternBank.db.models.Admin;
+import com.projeto.InternBank.db.models.Usuario;
 
 public interface AdminRepository extends JpaRepository <Admin, Long>{
 	Admin findById(String Id);
-	Admin findBySenha(String senha);
+	Admin findByUsuario(Usuario usuario);
+	//Admin findBySenha(String senha);
+	Usuario findByEmail(String email);
 }
